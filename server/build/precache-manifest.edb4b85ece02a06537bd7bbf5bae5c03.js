245bb9490e01e860f7ad743a1cb44ad3f7a9e28e
cruzador <batyrqh@protonmail.com>
2020-04-28 06:32:34 +0600

    build

---
+++ server/build/precache-manifest.edb4b85ece02a06537bd7bbf5bae5c03.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4271a6a1b81f4d82802f9d32cba3549c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9ec3ec12d61b284bca7",
+    "url": "/static/css/2.8d06fd47.chunk.css"
+  },
+  {
+    "revision": "a779a4c44f3ed749e06d",
+    "url": "/static/css/main.558e4b21.chunk.css"
+  },
+  {
+    "revision": "d9ec3ec12d61b284bca7",
+    "url": "/static/js/2.8b8b17df.chunk.js"
+  },
+  {
+    "revision": "13767bda7e58db6e95d0e3d841e04d61",
+    "url": "/static/js/2.8b8b17df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a779a4c44f3ed749e06d",
+    "url": "/static/js/main.2df149a0.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  }
+]);
